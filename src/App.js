9139f3e3fff9e2afa06ccfc79921d5ee9ad46296
cuixiaohui1202 <xiaohui@11bnb.com>
2018-04-26 16:34:11 +0800

    添加测试1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">欢迎来到React</h1>
         </header>
         <p className="App-intro">
-          我添加的这段字
+          我添加的这段字hhh
         </p>
       </div>
     );
