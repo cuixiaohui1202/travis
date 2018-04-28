5a307d694f143f8698fb194bb535f141890077b6
cuixiaohui1202 <xiaohui@11bnb.com>
2018-04-28 14:11:46 +0800

    测试1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">欢迎来到React</h1>
         </header>
         <p className="App-intro">
-            改一个文件
+            改一个文件改改改
         </p>
       </div>
     );
